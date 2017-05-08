2662553abc4ea41a8837ef0ded26f1145866f0e2
Aeshwer Tyagi <aeshwer.tyagi16@gmail.com>
2017-05-07 22:10:18 -0700

    DAY end data

--- LearningToCode/src/HelloWorld.java
+++ LearningToCode/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	
 	 StringBuffer str1 = new StringBuffer("Hello World");
     public static void main(String args[])
-    {  HelloWorld obj  =new HelloWorld();
+    {  HelloWorld obj  = new HelloWorld();
     	System.out.println(obj.str1 );
     }
 }
