55aad1b01ea703ca41d25822e6f5e0edef8373c4
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2018-05-04 17:53:01 +0530

    Basic threads

---
+++ ExecutorFramework/src/main/java/executorFramework1/FirstWay.java
@@ -0,0 +1,44 @@
+/*
+ * Creating Threads normally using thread API
+ */
+		
+package executorFramework1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.omg.CORBA.TIMEOUT;
+
+public class FirstWay {
+
+	public static void main(String[] args) {
+		System.out.println("MAIN THREAD START");
+		FirstTask firstTask = new FirstTask();
+		FirstTask secondTask = new FirstTask();
+		System.out.println("MAIN THREAD ENDS");
+		
+	}
+
+}
+	
+class FirstTask extends Thread
+{
+	private static int count= 0; 
+	private int id;
+	
+	@Override
+	public void run() {
+		for(int i=10;i>0;i--) {System.out.println("<" + id+ ">"+ " TICK TICK "+i);
+		
+		try {
+			TimeUnit.MILLISECONDS.sleep(200);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		}
+	}
+	
+	FirstTask()
+	{	this.id = ++count;
+		this.start();
+	}
+}
