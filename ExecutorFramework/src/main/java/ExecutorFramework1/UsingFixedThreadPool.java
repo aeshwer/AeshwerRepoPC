eb2dce229d580fa49b627435aed4092c3f6a8282
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2018-05-07 14:42:49 +0530

    Minro

---
+++ ExecutorFramework/src/main/java/ExecutorFramework1/UsingFixedThreadPool.java
@@ -0,0 +1,10 @@
+package ExecutorFramework1;
+
+public class UsingFixedThreadPool {
+
+	public static void main(String[] args) {
+		System.out.println("MAIN THREAD START");
+		
+		System.out.println("MAIN THREAD ENDS");
+	}
+}
