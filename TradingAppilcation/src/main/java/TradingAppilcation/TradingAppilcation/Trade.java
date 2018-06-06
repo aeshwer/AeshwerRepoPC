ca48013b205700799f2e2123039b549039a216b5
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2018-06-06 17:09:39 +0530

    sad

---
+++ TradingAppilcation/src/main/java/TradingAppilcation/TradingAppilcation/Trade.java
@@ -0,0 +1,46 @@
+package TradingAppilcation.TradingAppilcation;
+
+public class Trade {
+
+	private String id;
+	
+	private String BuySellIndicator;
+	
+	private int price1;
+	
+	private int price2;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getBuySellIndicator() {
+		return BuySellIndicator;
+	}
+
+	public void setBuySellIndicator(String buySellIndicator) {
+		BuySellIndicator = buySellIndicator;
+	}
+
+	public int getPrice1() {
+		return price1;
+	}
+
+	public void setPrice1(int price1) {
+		this.price1 = price1;
+	}
+
+	public int getPrice2() {
+		return price2;
+	}
+
+	public void setPrice2(int price2) {
+		this.price2 = price2;
+	}
+	
+	
+}
