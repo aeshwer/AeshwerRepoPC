36c6c80dd1f208bae72d5b7f1ee558107e1d7c0c
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2018-06-14 18:16:30 +0530

    as

---
+++ ExecutorFramework/src/main/java/ExtraOnMultiThreadingfWorld/Example2.java
@@ -0,0 +1,23 @@
+//Atomic Reference
+
+package ExtraOnMultiThreadingfWorld;
+import java.util.concurrent.atomic.AtomicReference;
+
+//Atomic reference should be used in a setting where you need to do simple atomic (i.e. thread safe, non-trivial) operations on a reference, 
+//for which monitor-based synchronization is not appropriate. 
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		String initialReference = "initial value referenced";
+		AtomicReference<String> atomicStringReference = new AtomicReference<String>(initialReference);
+
+		String newReference = "new value referenced";
+		boolean exchanged = atomicStringReference.compareAndSet(initialReference, newReference);
+		System.out.println("exchanged: " + exchanged);
+
+		exchanged = atomicStringReference.compareAndSet(initialReference, newReference);
+		System.out.println("exchanged: " + exchanged);
+	}
+
+}
