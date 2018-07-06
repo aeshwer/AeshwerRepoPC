e2cae082fd6231a1246a704a3ef5361f8f6d3cfb
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2018-07-06 15:25:31 +0530

    join method

---
+++ ExecutorFramework/src/main/java/WaitingForThreadToComplete/ValueReturningTaskC.java
@@ -0,0 +1,47 @@
+package WaitingForThreadToComplete;
+
+import java.util.concurrent.TimeUnit;
+
+// emulate long running calculation
+public class ValueReturningTaskC implements Runnable{
+
+	private int a;
+	private int b;
+	private long sleepTime;
+	private long sum;
+	
+	private static int count = 0;
+	private int instanceNumber = 0;
+	private String taskId;
+	
+	public ValueReturningTaskC(int a, int b,long sleepTime) {
+		this.a = a;
+		this.b = b;
+		this.sleepTime = sleepTime;
+		
+		this.instanceNumber = ++count;
+		this.taskId= "ValueReturningTaskC--" + instanceNumber;
+	}
+	
+	public void run() {
+		String currentThread =Thread.currentThread().getName();
+		
+		System.out.println("#### ["+ currentThread +"] <" + taskId + "> Starting #######" );
+		
+		System.out.println("["+ currentThread +"] <" + taskId + "> Sleeping for" + sleepTime +" milies" );
+		
+		try {
+			TimeUnit.MILLISECONDS.sleep(sleepTime);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		sum =a+b; // how to return this sum
+		System.out.println(" ******  ["+ currentThread +"] <" + taskId + "> Done milies" );
+
+	}
+	
+	public int getSum() 
+	{
+		return (int)sum;
+	}
+}
