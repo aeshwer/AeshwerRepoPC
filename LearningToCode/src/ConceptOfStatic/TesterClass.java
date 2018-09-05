97b697a365edc7815d506c1b298e36be82ec215f
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2018-09-05 18:02:39 +0530

    update

--- LearningToCode/src/ConceptOfStatic/TesterClass.java
+++ LearningToCode/src/ConceptOfStatic/TesterClass.java
@@ -4,6 +4,10 @@ public class TesterClass {
 	
 	public static void main(String[] args) {
 		
+		NonStaticClass class1 = new NonStaticClass();
+		
+		StaticClass class2 = new StaticClass();
+		
 	}
 
 }
