ded0d51e49154f99258057c82577bea8fbef9e8a
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2018-09-19 11:51:26 +0530

    update

--- Java8/src/Lesson2/test.java
+++ Java8/src/Lesson2/test.java
@@ -1,5 +1,15 @@
 package Lesson2;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class test {
 
+	public static void main(String[] args) {
+		Integer[] arr = new Integer[]{1,2,3,4,5}; 
+		List<Integer> list = Arrays.asList(arr);
+		System.out.println(list.toString());
+		list=list.stream().filter( element -> element > 3).collect(Collectors.toList());
+		System.out.println(list.toString());
+	}
 }
