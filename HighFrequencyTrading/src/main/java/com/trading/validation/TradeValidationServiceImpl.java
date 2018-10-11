874b258f683095f0403da8c85d607e1ce2e760b3
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2018-10-11 14:42:44 +0530

    Validation Guice Problem Fixed

--- HighFrequencyTrading/src/main/java/com/trading/validation/TradeValidationServiceImpl.java
+++ HighFrequencyTrading/src/main/java/com/trading/validation/TradeValidationServiceImpl.java
@@ -3,17 +3,22 @@ package com.trading.validation;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.inject.Inject;
-
+import com.google.inject.Inject;
 import com.trading.domain.response.TradeResponse;
 import com.trading.domain.trade.Trade;
 
 public class TradeValidationServiceImpl implements TradeValidationService{
 	
-	private TradeResponse response; 
+	private TradeResponse response;
+
+	@Inject
+	public TradeValidationServiceImpl() {
+	}
+	
 	
 	@Override
 	public TradeResponse validateTrade(Trade trade) {
+		response = new TradeResponse();
 		List<TradeValidationError> validationErrors = new ArrayList<>();
 		response.setValidationErrors(validationErrors);
 		response.setTrade(trade);
