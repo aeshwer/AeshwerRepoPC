d8927044bc7094c9723556e9ea63296a5eebba09
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2019-01-09 12:00:25 +0530

    Learing Guice ..use of provides with collections

---
+++ LearnGuice/src/LearnToInjectCollectionViaProvides/NotificationService.java
@@ -0,0 +1,23 @@
+package LearnToInjectCollectionViaProvides;
+
+import java.util.List;
+
+import com.google.inject.Inject;
+
+public class NotificationService {
+	
+	private List<INotificication> allStreamingNotifiers;
+
+	@Inject
+	public NotificationService(final List<INotificication> allStreamingNotifiers) {
+		this.allStreamingNotifiers = allStreamingNotifiers;
+	}
+
+	public void notifierAPI() {
+		for(INotificication notifier : allStreamingNotifiers ) 
+		{
+			notifier.sendNotification();
+		}
+	}
+
+}
