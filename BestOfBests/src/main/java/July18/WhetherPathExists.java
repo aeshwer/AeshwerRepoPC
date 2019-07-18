12cbf8b8e06941cb00c16dfdd6a46881bf4f7f02
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2019-07-18 18:21:52 +0530

    update

---
+++ BestOfBests/src/main/java/July18/WhetherPathExists.java
@@ -0,0 +1,86 @@
+//https://www.geeksforgeeks.org/find-whether-path-two-cells-matrix/
+
+package July18;
+
+import java.util.Scanner;
+
+public class WhetherPathExists {
+
+	public static class Pivot {
+		private int j;
+		private int i;
+
+		public Pivot(int i, int j) {
+			this.i = i;
+			this.j = j;
+		}
+	}
+
+	private static boolean isSafe(int i, int j, int n) {
+		if (i > n || j > n || i<0 || j<0) {
+			return false;
+		}
+		return true;
+	}
+
+	private static int resolvePath(int[][] arr, Pivot source, Pivot next, int n) {
+		/*for (int i = source.i; i < n; i++) {
+			for (int j = source.j; j < n; j++) {
+
+			}
+		}
+		return false;*/
+		
+		if(arr[next.i][next.j]==2) {return 1;}
+		
+		if(isSafe(source.i+1, source.j,n)) {resolvePath(arr, source, new Pivot(source.i+1, source.j), n);} 
+		if(isSafe(source.i, source.j+1,n)) {resolvePath(arr, source, new Pivot(source.i, source.j+1), n);}
+		if(isSafe(source.i-1, source.j,n)) {resolvePath(arr, source, new Pivot(source.i-1, source.j), n);}
+		if(isSafe(source.i, source.j-1,n)) {resolvePath(arr, source, new Pivot(source.i, source.j-1), n);}
+		
+		return 0;
+		
+
+	}
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int cases = s.nextInt();
+		Pivot source=null;
+		Pivot destination=null;
+		while(--cases>=0) 
+		{
+			int n = s.nextInt();
+			int arr[][] = new int[n][n];
+			
+			int arr1[] = new int[n*n];
+			int temp =n*n;
+			int c=0;
+			while(--temp>=0) 
+			{
+				arr1[c++]=s.nextInt();
+			}
+			c=0;
+			for(int i=0;i<n;i++) {
+				for(int j=0;j<n;j++) {
+					arr[i][j]=arr1[c];
+					if(arr1[c]==1) 
+					{
+						source = new Pivot(i, j);
+						
+					}
+					c++;
+					/*if(s.nextInt()==2) 
+					{
+						 destination = new Pivot(i, j);
+						
+					}*/
+				}
+			}
+			int res = resolvePath(arr,source,source,n);
+			System.out.println(res);
+		}
+		
+
+	}
+}
