9dc0e0138e30af989c067efadac4a3ad4a7e1e33
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2019-07-19 18:02:00 +0530

    update

--- BestOfBests/src/main/java/July19/BreathFirstSearchForConnectedGraph.java
+++ BestOfBests/src/main/java/July19/BreathFirstSearchForConnectedGraph.java
@@ -1,6 +1,7 @@
 //https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
 package July19;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class BreathFirstSearchForConnectedGraph {
@@ -18,14 +19,45 @@ public class BreathFirstSearchForConnectedGraph {
 	
 
     // Function to add an edge into the graph 
-    void addEdge(int startVertices,int endVertices) 
+    void addEdge(int verticesIndex,int adjVertices) 
     { 
-        adj[startVertices].add(endVertices); 
+        adj[verticesIndex].add(adjVertices); 
     } 
 
     // prints BFS traversal from a given source s 
 	private void BFS(int startIndex) {
 
+        // Mark all the vertices as not visited(By default 
+        // set as false) 
+        boolean visited[] = new boolean[vertices]; 
+  
+        // Create a queue for BFS 
+        LinkedList<Integer> queue = new LinkedList<Integer>(); 
+  
+        // Mark the current node as visited and enqueue it 
+        visited[startIndex]=true; 
+        queue.add(startIndex); 
+  
+        while (queue.size() != 0) 
+        { 
+            // Dequeue a vertex from queue and print it 
+            startIndex = queue.poll(); 
+            System.out.print(startIndex+" "); 
+  
+            // Get all adjacent vertices of the dequeued vertex s 
+            // If a adjacent has not been visited, then mark it 
+            // visited and enqueue it 
+            Iterator<Integer> i = adj[startIndex].listIterator(); 
+            while (i.hasNext()) 
+            { 
+                int n = i.next(); 
+                if (!visited[n]) 
+                { 
+                    visited[n] = true; 
+                    queue.add(n); 
+                } 
+            } 
+        } 
 		
 	}
 	public static void main(String[] args) {
---
+++ BestOfBests/src/main/java/July19/DepthFirstSearchForDisconnectedGraph.java
@@ -0,0 +1,70 @@
+//https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
+package July19;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class DepthFirstSearchForDisconnectedGraph {
+
+	private int vertices; // No. of vertices
+	private LinkedList<Integer> adj[]; // Adjacency Lists
+
+	// Constructor
+	public DepthFirstSearchForDisconnectedGraph(int vertices) {
+		this.vertices = vertices;
+		this.adj = new LinkedList[vertices];
+		for (int i = 0; i < vertices; ++i)
+			adj[i] = new LinkedList();
+	}
+
+	// Function to add an edge into the graph
+	void addEdge(int verticesIndex, int adjVertices) {
+		adj[verticesIndex].add(adjVertices);
+	}
+
+	// prints BFS traversal from a given source startIndex
+	private void DFSUtil(int startIndex, boolean visited[]) {
+
+		// Mark the current node as visited and print it
+		visited[startIndex] = true;
+		System.out.print(startIndex + " ");
+
+		// Recur for all the vertices adjacent to this vertex
+		Iterator<Integer> i = adj[startIndex].listIterator();
+		while (i.hasNext()) {
+			int n = i.next();
+			if (!visited[n])
+				DFSUtil(n, visited);
+		}
+
+	}
+
+	// The function to do DFS traversal. It uses recursive DFSUtil()
+	void DFS() {
+		// Mark all the vertices as not visited(set asstartIndex
+		// false by default in java)
+		boolean visited[] = new boolean[vertices];
+
+		// Call the recursive helper function to print DFS traversal 
+        // starting from all vertices one by one 
+        for (int i=0; i<vertices; ++i) 
+            if (visited[i] == false) 
+                DFSUtil(i, visited); 
+	}
+
+	public static void main(String[] args) {
+		DepthFirstSearchForDisconnectedGraph g = new DepthFirstSearchForDisconnectedGraph(4);
+
+		g.addEdge(0, 1);
+		g.addEdge(0, 2);
+		g.addEdge(1, 2);
+		g.addEdge(2, 0);
+		g.addEdge(2, 3);
+		g.addEdge(3, 3);
+
+		System.out.println("Following is Breadth First Traversal " + "(starting from vertex 2)");
+
+		g.DFS();
+	}
+
+}
