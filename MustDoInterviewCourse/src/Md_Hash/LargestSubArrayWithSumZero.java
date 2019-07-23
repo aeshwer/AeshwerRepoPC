35ac73e5d079b5cd94708b511df51d9328a667e6
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2019-07-23 17:47:21 +0530

    update

--- MustDoInterviewCourse/src/Md_Hash/LargestSubArrayWithSumZero.java
+++ MustDoInterviewCourse/src/Md_Hash/LargestSubArrayWithSumZero.java
@@ -3,6 +3,9 @@ package Md_Hash;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class LargestSubArrayWithSumZero {
@@ -32,8 +35,22 @@ public class LargestSubArrayWithSumZero {
 		}
 	}
 
-	int maxLen(int arr[], int n) {
-		return 0;
+	static int maxLen(int arr[], int n) {
+		Map<Integer, Integer> lookup = new HashMap<Integer, Integer>();
+		lookup.put(arr[0], 0);
+		int maxLen = 1;
+		int sum =arr[0];
+		for(int i=1;i<n;i++) {
+			
+			sum= sum+arr[i];
+			if(sum==0) 
+			{
+				maxLen=Math.max(maxLen, lookup.get(sum)-i);
+			}
+			
+		}
+		
+		return maxLen;
 	}
 
 	public static void main(String[] args) {
@@ -41,10 +58,15 @@ public class LargestSubArrayWithSumZero {
 		int cases = reader.nextInt();
 
 		while (--cases >= 0) {
-		
+
 			int n = reader.nextInt();
-			int arr[] =
-			
+			int arr[] = new int[n];
+			int i = 0;
+			while (--n >= 0) {
+				arr[i++] = reader.nextInt();
+			}
+
+			maxLen(arr, arr.length);
 		}
 	}
 
