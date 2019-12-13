163d9d3dd2ad868b82c69a949dccf140a0ebdcf1
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2019-12-13 18:41:35 +0530

    KMP algo learn

---
+++ DynamicPrograming/src/LatestProg/KMPPatternMatchingAlogrithm.java
@@ -0,0 +1,68 @@
+package LatestProg;
+
+import java.util.Arrays;
+
+//O(n+m)
+public class KMPPatternMatchingAlogrithm {
+
+	public static void main(String[] args) {
+		String s1 = "dsgwadsgz";
+		String s2 = "dsgwadsgz";
+		isMatchingPattern(s1.toCharArray(), s2.toCharArray());
+	}
+
+	private static void isMatchingPattern(char[] inputString, char[] pattern) {
+
+		// Make Suffix Prefix Array O(n)
+		int count[] = new int[pattern.length];
+		count[0] = 0;
+
+		int i = 0;
+		for (int j = 1; j < pattern.length; j++) {
+			if (pattern[j] == pattern[i]) {
+				count[j] = i + 1;
+				i++;
+			} else {
+				count[j] = i - 1 > 0 ? count[i - 1] : 0;
+			}
+
+		}
+		// System.out.println(Arrays.toString(count));
+
+		// Now Make use of this suffix-Prefix Table To compute the Substring check
+		// efficiently
+
+		/*
+		 * int patternCountIndex = 0; for (i = 0; i < inputString.length; i++) { if
+		 * (patternCountIndex == pattern.length) { result = true; break; } if
+		 * (inputString[i] == pattern[patternCountIndex]) { patternCountIndex++; } else
+		 * { patternCountIndex = count[patternCountIndex]; i--; } }
+		 */
+
+		i = 0;
+		int j = 0;
+		while (i < inputString.length && j < pattern.length) {
+			if (inputString[i] == pattern[j]) {
+				i++;
+				j++;
+			}
+			else {
+				if(j!=0) {
+					j=count[j-1];
+				}
+				else {
+					i++;
+				}
+				
+			}
+
+		}
+
+		if (j==pattern.length) {
+			System.out.println("Substring Pattern Matched");
+		} else {
+			System.out.println("Substring Pattern Not Matched");
+		}
+
+	}
+}
