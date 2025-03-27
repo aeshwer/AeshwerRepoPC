7bf6cacf3fc7445a8be0828e37169adfc507a9d2
Aeshwer Tyagi <AESHWER.TYAGI@salesforce.com>
2025-03-28 00:41:58 +0530

    com

--- ConcurencyLearning/src/com/udemyConcurreny/App.java
+++ ConcurencyLearning/src/com/udemyConcurreny/App.java
@@ -1,38 +1,41 @@
 package com.udemyConcurreny;
 
-
-class runner1 implements Runnable
-{
-	public void run(){
-	for(int i=0;i<10;i++){System.out.println("Runner1");}
+class runner1 implements Runnable {
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Runner1");
 		}
-}
-
-class runner2 implements Runnable
-{
-	public void run(){
-	for(int i=0;i<10;i++){System.out.println("Runner2");}
 	}
 }
 
+class runner2 implements Runnable {
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Runner2");
+		}
+	}
+}
 
-
-public class App {	
+public class App {
 	public static void main(String[] args) {
 
-		Thread t1  = new Thread(new runner1());
-		Thread t2  = new Thread(new runner2());
-		
+		Thread t1 = new Thread(new runner1());
+		Thread t2 = new Thread(new runner2());
+
+		// start: method begins the execution of a new thread
 		t1.start();
 		t2.start();
+		// Join:allows one thread to wait for another thread to finish execution before
+		// continuing.
 		try {
+			// Main thread waits until t1 is done
 			t1.join();
+			// Main thread waits until t2 is done
 			t2.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		System.out.println("Finished");
 	}
 
